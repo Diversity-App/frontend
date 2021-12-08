45c226a24b5d3ca658aea37f1d3532e587e07947
Corentin MAS <corentinmas@yahoo.com>
2021-12-08 10:36:07 +0100

    [feature/ci] Re-init project

--- ios/diversity_appTests/diversity_appTests.m
+++ ios/diversity_appTests/diversity_appTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface diversityFrontTests : XCTestCase
+@interface diversity_appTests : XCTestCase
 
 @end
 
-@implementation diversityFrontTests
+@implementation diversity_appTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
