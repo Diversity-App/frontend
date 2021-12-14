e1eba34a49020dde5c058cdfa870663d1c5f9b74
Axel Fuentes <axel.fuentes@epitech.eu>
2021-12-14 22:52:03 +0100

    [ADD] diagramme ok

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -5,21 +5,41 @@ import Header from '../components/Header';
 import Paragraph from '../components/Paragraph';
 // import Button from '../components/Button';
 import { Navigation } from '../types';
-import { TouchableOpacity, StyleSheet, Text, View, Button } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, View, Button, ScrollView, StatusBar } from 'react-native';
+import PieChart from 'react-native-pie-chart';
 
 type Props = {
     navigation: Navigation;
 };
 
-const Dashboard = ({ navigation }: Props) => (
-    <Background>
-        <Logo />
-        <Header>Let’s start</Header>
-        <Paragraph>
-            Let's go for the code !!
-        </Paragraph>
-        <Button title="Logout" onPress={() => navigation.navigate('HomeScreen')} />
-    </Background>
-);
+const Dashboard = ({ navigation }: Props) => {
+    const widthAndHeight = 250
+    const series = [123, 321, 123, 789, 537]
+    const sliceColor = ['#F44336', '#2196F3', '#FFEB3B', '#4CAF50', '#FF9800']
+
+    return (
+        <Background>
+        <ScrollView style={{display: 'flex', marginTop: 450 }}>
+            <View style={styles.container}>
+                <Text style={{color: 'white', fontSize: 40}}>Statistique</Text>
+                <PieChart
+                    widthAndHeight={widthAndHeight}
+                    series={series}
+                    sliceColor={sliceColor}
+                />
+            </View>
+        </ScrollView></Background>)
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 24,
+        margin: 10
+    }
+});
 
 export default memo(Dashboard);
