3114cee646ee028a83dad7558ec1a23f015c0bfb
Corentin MAS <corentinmas@yahoo.com>
2022-01-28 13:38:05 +0100

    [feature/youtube-sso] Changed new google auth redirect uri strategy

--- src/screens/ConnectAccounts.tsx
+++ src/screens/ConnectAccounts.tsx
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Navigation } from "../types";
 import Background from "../components/Background";
 import Logo from "../components/Logo";
-import Header from "../components/Header";
-import { AnimatedCircularProgress } from "react-native-circular-progress";
 import { Button } from "react-native-paper";
-import { makeRedirectUri, Prompt, useAuthRequest } from "expo-auth-session";
+import { makeRedirectUri, Prompt, startAsync } from "expo-auth-session";
 // import Button from '../components/Button';
 import localStorage from "../utils/localStorage";
+import axios from "axios";
 
 type Props = {
   navigation: Navigation & any;
@@ -16,13 +15,49 @@ type Props = {
 
 
 const ConnectAccounts = ({ navigation, apiToken }: Props) => {
-  function connectYoutube() {
-    navigation.navigate("YoutubeConnexion", { apiToken })
+   const connectYoutube = async () => {
+    // navigation.navigate("YoutubeConnexion", { apiToken });
+
+
+    const authParams = {
+      client_id: "1048252460044-n911v4bjibafcdml211oqkco66bb61b5.apps.googleusercontent.com",
+      redirect_uri: makeRedirectUri({
+        // native: "myapp://redirect",
+        useProxy: true
+      }),
+      scope: "https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/userinfo.profile",
+      response_type: "code",
+      prompt: Prompt.Consent
+    };
+    const authURL = `https://accounts.google.com/o/oauth2/v2/auth?${new URLSearchParams(authParams).toString()}`;
+
+    // @ts-ignore
+    const { type, params } = await startAsync({ authUrl: authURL });
+
+
+    if (type !== "success") {
+      return;
+    }
+
+    const { code } = params;
+
+    await axios.get("http://localhost:8080/api/auth/sso/google/callback", {
+      params: {
+        code: code
+      },
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + apiToken
+      }
+    });
+
+    localStorage.setItem("YoutubeConnexion", "true");
   }
-  const isConnected = localStorage.getItem("YoutubeConnexion")
+
+  const isConnected = localStorage.getItem("YoutubeConnexion");
 
   if (isConnected === "true") {
-    navigation.navigate("Dashboard")
+    navigation.navigate("Dashboard");
   }
 
   return (
--- src/screens/YoutubeConnexion.ts
+++ src/screens/YoutubeConnexion.ts
@@ -1,10 +1,9 @@
 import { useEffect } from "react";
-import { makeRedirectUri, Prompt, useAuthRequest } from "expo-auth-session";
+import { makeRedirectUri, Prompt, useAuthRequest, startAsync } from "expo-auth-session";
 import Background from "../components/Background";
 import axios from "axios";
 import { Navigation } from "../types";
 import localStorage from "../utils/localStorage";
-
 
 type Props = {
   navigation: Navigation;
@@ -12,58 +11,59 @@ type Props = {
 };
 
 const YoutubeConnexion = ({ navigation, apiToken }: Props) => {
-  const params = {
-    clientId: "477634730335-4tuj7dprmu3m88m600qnm722ico8g1df.apps.googleusercontent.com",
-    redirectUri: makeRedirectUri({
-      native: "myapp://redirect"
+
+  const authParams = {
+    client_id: "1048252460044-n911v4bjibafcdml211oqkco66bb61b5.apps.googleusercontent.com",
+    redirect_uri: makeRedirectUri({
+      // native: "myapp://redirect",
+      useProxy: true,
     }),
-    scopes: ["https://www.googleapis.com/auth/youtube","https://www.googleapis.com/auth/userinfo.profile"],
+    scope: "https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/userinfo.profile",
     response_type: "code",
-    prompt: Prompt.Consent
+    prompt: Prompt.Consent,
   };
-  const url = {
-    authorizationEndpoint: "https://accounts.google.com/o/oauth2/auth"
-  };
+  const authURL = `https://accounts.google.com/o/oauth2/v2/auth?${new URLSearchParams(authParams).toString()}`;
 
+  // const {type, params } = await startAsync({authUrl: authURL})
 
-  const [request, response, promptAsync] = useAuthRequest(params, url);
-
-  useEffect(() => {
-    if (!response) {
-      return;
-    }
-
-
-    if (response?.type !== "success") {
-      return navigation.navigate("ConnectAccounts");
-    }
-
-    // @ts-ignore
-    const { code } = response.params;
-
-    axios.get("http://localhost:8080/api/auth/sso/google/callback", {
-      params: {
-        code: code
-      },
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + apiToken
-      }
-    })
-      .then(res => {
-        // Store success status in local storage
-        localStorage.setItem("YoutubeConnexion", "true");
-
-        navigation.navigate("ConnectAccounts");
-      })
-      .catch(err => {
-        console.log(err);
-      });
-
-  }, [response]);
+  // const [request, response, promptAsync] = useAuthRequest(params, url);
 
   // useEffect(() => {
-    promptAsync();
+  //   if (!response) {
+  //     return;
+  //   }
+  //
+  //
+  //   if (response?.type !== "success") {
+  //     return navigation.navigate("ConnectAccounts");
+  //   }
+  //
+  //   // @ts-ignore
+  //   const { code } = response.params;
+  //
+  //   axios.get("http://localhost:8080/api/auth/sso/google/callback", {
+  //     params: {
+  //       code: code
+  //     },
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Authorization": "Bearer " + apiToken
+  //     }
+  //   })
+  //     .then(res => {
+  //       // Store success status in local storage
+  //       localStorage.setItem("YoutubeConnexion", "true");
+  //
+  //       navigation.navigate("ConnectAccounts");
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  //
+  // }, [response]);
+
+  // useEffect(() => {
+  //   promptAsync();
   // }, []);
 
   return null;
